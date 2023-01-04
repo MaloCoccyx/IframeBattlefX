a5d687ec5803634a3178b92cfa719f0f13c379a9
MaloCoccyx <lequartguillaume3@gmail.com>
2023-01-04 16:41:25 +0100

    Add manual control and animations (click on buttons -> change class to animate)

--- js/script.js
+++ js/script.js
@@ -7,185 +7,83 @@
 ##                            ##
 ################################
 */
-
-/*
-#################################
-## Initialize Global Variables ##
-#################################
-*/
-//const IMG = document.querySelector("displayRobot");
-//const IMGIFRAME = document.querySelector("iFrameDisplayRobot");
-const ROBOTBOX = document.getElementById("robotBox");
-const BUTTONON = document.getElementById("modeAutoOn");
-const BUTTONOFF = document.getElementById("modeAutoOff");
-const SOUNDON = document.getElementById("soundOn");
-const SOUNDOFF = document.getElementById("soundOff");
-
-var moveAndRotate = [
-    document.querySelector('#turnToRight'), document.querySelector('#turnToLeft'), document.querySelector('#moveToRight'), 
-    document.querySelector('#moveToForward'), document.querySelector('#moveToLeft'), document.querySelector('#moveToBackward'), 
-    document.querySelector('#shooting')
-];
-
-/*
-######################################
-## Set Button to Enabled / Disabled ##
-######################################
-/* Enable / Disable buttons "modeAuto" */
-function modeAutoOnOff(){
-
-    // Treatment
-    if(BUTTONOFF.className == "disabled" && BUTTONON.className == "enabled"){
-
-        // Disable Controls
-        moveAndRotate[0].disabled = true; // turnToRight
-        moveAndRotate[1].disabled = true; // turnToLeft
-        moveAndRotate[2].disabled = true; // moveToRight
-        moveAndRotate[3].disabled = true; // moveToForward
-        moveAndRotate[4].disabled = true; // moveToLeft
-        moveAndRotate[5].disabled = true; // moveToBackward
-        moveAndRotate[6].disabled = true; // button to shoot
-        modeAuto = true;                 // set modeAuto to True
-        BUTTONOFF.className = "enabled"; // enable button modeAutoOff
-        BUTTONON.className = "disabled"; // disable button modeAutoOn
-
-        return;
-    }else if(BUTTONOFF.className == "enabled" && BUTTONON.className == "disabled"){
-
-        // Enable Controls
-        moveAndRotate[0].disabled = false;
-        moveAndRotate[1].disabled = false;
-        moveAndRotate[2].disabled = false;
-        moveAndRotate[3].disabled = false;
-        moveAndRotate[4].disabled = false;
-        moveAndRotate[5].disabled = false;
-        moveAndRotate[6].disabled = false;
-        modeAuto = false;                 // set modeAuto to False
-        BUTTONOFF.className = "disabled"; // disable button modeAutoOff
-        BUTTONON.className = "enabled"; // enable button modeAutoOn
-
-        return;
-    }
-}
-
-/* Enable / Disable buttons "sound" */
-function soundOnOff(){
-
-    // Treatment
-    if(SOUNDOFF.className == "disabled" && SOUNDON.className == "enabled"){
-        SOUNDOFF.className = "enabled";
-        SOUNDON.className = "disabled";
-        return;
-    }else if(SOUNDOFF.className == "enabled" && SOUNDON.className == "disabled"){
-        SOUNDOFF.className = "disabled";
-        SOUNDON.className = "enabled";
-        return;
-    }
-}
-
-/*
-
-function displayRobotState(){
-    // Treatment
-    if(ROBOTBOX.className != "flex " + robotState){
-        ROBOTBOX.className = "flex " + robotState;
-        setTimeout(() => {
-            // If paremeter != "isDead", robot still alive
-            if(robotState != 'isDead'){
-                ROBOTBOX.className = "flex isAlive";
-                robotState = "isAlive";
-            }else{
-                ROBOTBOX.className = "flex isDead";
-                robotState = "isDead";
-            }
-            }, 1000
-        );
-        return;
-    }
-}
-
-/*
-###################
-## Robot MoveToX ##
-###################
-
-/* Robot MoveToX (Forward, Left, Right, Backward)*/
-function isMoveTo(Parameter){
-
-    // Treatment
-    ROBOTBOX.classList.remove("isMoveToForward");
-    ROBOTBOX.classList.remove("isMoveToLeft");
-    ROBOTBOX.classList.remove("isMoveToRight");
-    ROBOTBOX.classList.remove("isMoveToBackward");
-    setTimeout(() => {
-            ROBOTBOX.classList.add("isMoveTo" + Parameter);
-            if(Parameter == "forward"){
-                marioBot = move
-            }else if(Parameter == "forward"){
-                
-            }else if(Parameter == "forward"){
-                
-            }else if(Parameter == "forward"){
-                
-            }
-        }, 1
-    );
-    return;
-}
-
-
-/*
-########################
-## Change Robot State ##
-########################
-*/
-function stateChanged(Parameter){
-    robotState = Parameter;
-    if(life <= 0){
-        robotState = "isDead";
-        displayRobotState(robotState);
-        robotDead = true;
-    }else if(fire = true){
-        displayRobotState(robotState);
-    }else if(isTakeDamage == true){
-        robotState = "isTakeDamage";
-        displayRobotState(robotState);
-    }
-}
-
-/**
- * 
- * @param {Event} event 
- */
-function onLoaded(event){
-    console.log(event);
-
-    let agent = new Agent(
-        "guillaume_lequart", // id
-        "demo", // username
-        "demo", // password
-        "iframebattlefx", // arena 
-        "8080", // port
-        "mqtt.jusdeliens.com", // server
-        4, // verbose
-        false // true
-    );
-    agent.connect();
-    agent.addEventListener("connected", onConnected);
-    agent.addEventListener("updated", onUpdated);
-    agent.addEventListener("dirChanged", onDirChanged)
-}
+var agent;
+var robotBox = document.querySelector("#robotBox");
 
 /**
  * 
  * @param {AgentEvent} event 
  */
 function onConnected(event){
-    let agent = event.src;
     console.log("Agent connected " + agent.id);
     document.getElementById("idRobot").textContent = agent.id; // Get Robot Name
+    robotBox.className = "flex isBorn";
 
+}
+
+/**
+ * 
+ * @param {AgentEvent} event 
+ */
+function onUpdated(event){
+    console.log("Updated Agent " + agent.id);
+}
+
+/**
+ * 
+ * @param {AgentEvent} event 
+ */
+function onDirChanged(event){
+    let img = document.querySelector("#displayRobot");
+    let imgIframe = document.querySelector("#iFrameDisplayRobot");
+
+	img.style.transform = "rotate("+agent.dir*90+"deg)";
+    imgIframe.style.transform = "rotate("+agent.dir*90+"deg)";
+}
+
+/**
+ * 
+ * @param {AgentEvent} event 
+ */
+function onLifeChanged(event){
     /**
+     * @var {number}  percentlife Calculate the percentage of life
+     * @var {string}  lifeBg Change the background gradient uses percentAmmo as value for percentage
+     * @var {Array}   life Get all elements of ammo bar (responsive elements) and put it into array to easily modify them
+     */
+    let percentlife = (((agent.life) / 100) * 100);
+    let lifeBg = "linear-gradient(to right, var(--is-dead) " + percentlife +"%,transparent " + percentlife +"%)";
+
+    let life = [
+        document.getElementById("life"),
+        document.getElementById("lifeShort"),
+        document.getElementById("lifeIframe")
+    ];
+
+    let oldLife = life;
+
+    life[0].textContent = "Vie : " + (((agent.life) / 100) * 100) + " / " + "100";
+    life[0].style.background = lifeBg;
+    life[1].textContent = "V: " + (((agent.life) / 100) * 100) + " / " + "100";
+    life[1].style.background = lifeBg;
+    life[2].textContent = (((agent.life) / 100) * 100) + " / " + "100";
+    life[2].style.background = lifeBg;
+
+    if(life <= 0){
+       robotBox.className = "flex isDead";
+    }
+
+    if(oldLife < life){
+        robotBox.className = "flex isTakeDamage";
+    }
+}
+
+/**
+ * 
+ * @param {AgentEvent} event 
+ */
+function onAmmoChanged(event){
+    /*
      * @var {number}  percentAmmo Calculate the percentage of ammo
      * @var {string}  ammoBg Change the background gradient uses percentAmmo as value for percentage
      * @var {Array}   ammo Get all elements of ammo bar (responsive elements) and put it into array to easily modify them
@@ -209,79 +107,117 @@ function onConnected(event){
     ammo[3].textContent = (((agent.ammo) / 100) * 100) + "/" + "100";
     ammo[3].style.background = ammoBg;
 
-    /**
-     * @var {number}  percentlife Calculate the percentage of life
-     * @var {string}  lifeBg Change the background gradient uses percentAmmo as value for percentage
-     * @var {Array}   life Get all elements of ammo bar (responsive elements) and put it into array to easily modify them
-     */
-    let percentlife = (((agent.life) / 100) * 100);
-    let lifeBg = "linear-gradient(to right, var(--is-dead) " + percentlife +"%,transparent " + percentlife +"%)";
+}
+function removeAnimClass(params){
+    let robotBox = document.querySelector("#robotBox").classList;
+    robotBox.remove("isMoveToForward");
+    robotBox.remove("isMoveToBackward");
+    robotBox.remove("isMoveToLeft");
+    robotBox.remove("isMoveToRight");
+    robotBox.remove("isShooting");
+    robotBox.remove("isTakeDamage");
 
-    let life = [
-        document.getElementById("life"),
-        document.getElementById("lifeShort"),
-        document.getElementById("lifeIframe")
-    ];
-
-    life[0].textContent = "Vie : " + (((agent.life) / 100) * 100) + " / " + "100";
-    life[0].style.background = lifeBg;
-    life[1].textContent = "V: " + (((agent.life) / 100) * 100) + " / " + "100";
-    life[1].style.background = lifeBg;
-    life[2].textContent = (((agent.life) / 100) * 100) + " / " + "100";
-    life[2].style.background = lifeBg;
+    if(params != null) robotBox.add(params);
 }
 
-
-
-/**
- * 
- * @param {AgentEvent} event 
- */
-function onUpdated(event){
-
-    let agent = event.src;
-    console.log("Updated Agent" + agent.id);
-
-    agent.lookTo((agent.dir + 2)%4);
+function isShooting(event){
+    agent.fire(true);
+    removeAnimClass("isShooting");
+    console.log("Is Shooting!");
 }
 
-function onDirChanged(event){
-    let agent = event.src;
-    console.log(agent.dir);
+function stopShooting(event){
+    agent.fire(false);
+    console.log("Stop shooting!");
+}
 
-    let IMG = document.querySelector("#displayRobot");
-    let IMGIFRAME = document.querySelector("#iFrameDisplayRobot");
+function moveToForward(event){
+    agent.move(0,-1);
+    removeAnimClass("isMoveToForward");
+    console.log("Déscend d'une case");
+}
 
-    switch(agent.dir){
-        case 0:
-            IMG.className = "forward";
-            IMG.src = "img/mario/robot-forward.svg";
+function moveToBackward(event){
+    agent.move(0,1);
+    removeAnimClass("isMoveToBackward");
+    console.log("Monte d'une case");
+}
 
-            IMGIFRAME.className = "forward";
-            IMGIFRAME.src = "img/mario/robot-forward.svg";
-            break;
-        case 1:
-            IMG.className = "backward";
-            IMG.src = "img/mario/robot-backward.svg";
+function moveToLeft(event){
+    agent.move(-1, 0);
+    removeAnimClass("isMoveToLeft");
+    console.log("A gauche d'une case");
+}
 
-            IMGIFRAME.className = "backward";
-            IMGIFRAME.src = "img/mario/robot-backward.svg";
-            break;
-        case 2:
-            IMG.className = "left";
-            IMG.src = "img/mario/robot-left.svg";
+function moveToRight(event){
+    agent.move(1, 0);
+    removeAnimClass("isMoveToRight");
+    console.log("A droite d'une case");
+}
 
-            IMGIFRAME.className = "left";
-            IMGIFRAME.src = "img/mario/robot-left.svg";
-            break;
-        case 3:
-            IMG.className = "right";
-            IMG.src = "img/mario/robot-right.svg";
-            
-            IMGIFRAME.className = "right";
-            IMGIFRAME.src = "img/mario/robot-right.svg";
-            break;
+function turnToRight(event){
+    agent.lookTo(agent.dir + 1);
+    console.log("Se tourne vers la droite");
+}
+
+function turnToLeft(event){
+    if(agent.dir <= 0 || agent.dir >= 4){
+        agent.lookTo(0);
+    }else{
+        agent.lookTo(agent.dir - 1);
+        console.log("Se tourne vers la gauche " + agent.dir);
     }
 }
 
+/**
+ * 
+ * @param {Event} event 
+ */
+function onLoaded(event){
+
+    let fireButton = document.querySelector("#shooting")
+    fireButton.addEventListener('mousedown', isShooting);
+    fireButton.addEventListener('mouseup', stopShooting);
+
+    document.querySelector("#moveToBackward").addEventListener('click', moveToBackward);
+    document.querySelector("#moveToRight").addEventListener('click', moveToRight);
+    document.querySelector("#moveToForward").addEventListener('click', moveToForward);
+    document.querySelector("#moveToLeft").addEventListener('click', moveToLeft);
+
+    document.querySelector("#turnToLeft").addEventListener('click', turnToLeft);
+    document.querySelector("#turnToRight").addEventListener('click', turnToLeft);
+
+
+    let url = new URLSearchParams(window.location.search);
+    console.log(event);
+
+    let id = url.get('agentid');
+    let readonly = url.get('readonly');
+    let verbosity = url.get('verbosity');
+
+    readonly = ( readonly == null ) ? true : (readonly === 'true');
+	verbosity = ( verbosity == null ) ? 1 : parseInt(verbosity);
+	console.log(`Creating agent ...`);
+	console.log(`readonly:${readonly}`);
+	console.log(`verbosity:${verbosity}`);
+
+    agent = new Agent(
+        "guillaume_lequart", // id
+        "demo", // username
+        "demo", // password
+        "iframebattlefx", // arena 
+        "8080", // port
+        "mqtt.jusdeliens.com", // server
+        verbosity, // verbose, 0, 1, 2, 3, 4
+        readonly // true or false
+    );
+
+    agent.connect();
+    agent.addEventListener("connected", onConnected);
+    agent.addEventListener("updated", onUpdated);
+    agent.addEventListener("dirChanged", onDirChanged);
+
+    agent.addEventListener('lifeChanged', onLifeChanged);
+    agent.addEventListener('ammoChanged', onAmmoChanged);
+}
 document.addEventListener("DOMContentLoaded", onLoaded);
